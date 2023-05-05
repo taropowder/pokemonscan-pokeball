121bc34c4b95ad56dccfd75096400a414e9e9e8b
taro <taropowder@gmail.com>
2023-05-05 23:02:33 +0800

    xray 独立运行版本

--- images/xray/run.sh
+++ images/xray/run.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-# 循环执行 /app/xray
-while true
-do
-    echo "xray $@"
-    /app/xray $@
-    echo "xray has been killed, restarting..."
-done
+
+PARAMETER=$@
+
+# 如果 ARGS 中包含 listen 字符串
+if [[ $PARAMETER =~ "listen" ]]; then
+    while true
+    do
+        echo "xray $PARAMETER"
+        sleep 1
+        /app/xray $PARAMETER
+        echo "xray has been killed, restarting..."
+    done
+else
+  echo "xray $PARAMETER"
+  /app/xray PARAMETER
+fi
+
