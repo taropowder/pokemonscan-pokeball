b8c3a69ff6bec36378649fd45cc5c0bf0d05a53b
taro <taropowder@gmail.com>
2023-05-31 16:24:45 +0800

    fix libc

--- script/build_pokeball.sh
+++ script/build_pokeball.sh
@@ -1,7 +1,8 @@
 mkdir -p build/pokeball/bin
 mkdir -p build/pokeball/attachment
 gitDescribe=$(git log --pretty=oneline --abbrev-commit -1)
-go build  -ldflags "-X 'main.gitDescribe=${gitDescribe}'" -buildvcs=auto  -o ./build/pokeball/bin/pokeball ./src/cmd
+git config --global --add safe.directory /__w/pokemonscan-pokeball/pokemonscan-pokeball
+go build  -ldflags "-X 'main.gitDescribe=${gitDescribe}'"  -o ./build/pokeball/bin/pokeball ./src/cmd
 cp script/pokeball.service build/pokeball/attachment
 cp script/pokeball.sh build/pokeball/bin
 chmod +x build/pokeball/bin/*
